8a9c2151f7866f4be32f332b6b71f504d662372a
doworkwhile <doworkwhile@protonmail.com>
2019-05-16 15:10:53 -0700

    quick sort with median of left-mid-right optimization

--- c++/quick_sort/quick_sort.cpp
+++ c++/quick_sort/quick_sort.cpp
@@ -10,23 +10,65 @@ void fillArray(int* ary, int max, int valRange)
   }
 }
 
-void printArray(int* ary, int from, int to)
+void printArray(int* ary, int max)
 {
-  int i = from;
-  for (; i < to - 1; ++i) {
+  for (int i = 0; i < max; i++) {
     cout << ary[i] << " ";
   }
-  cout << ary[i] << endl;
+  cout << endl;
+}
+
+void manualSort(int* ary, int start, int end)
+{
+  int size = (end - start) + 1;
+  if (size == 1) {
+    return;
+  }
+
+  if (size == 2 && ary[start] > ary[end]) {
+    swap(ary[start], ary[end]);
+  } else if (size == 3) {
+    if (ary[start] > ary[end - 1]) {
+      swap(ary[start], ary[end - 1]);
+    }
+
+    if (ary[start] > ary[end]) {
+      swap(ary[start], ary[end]);
+    }
+
+    if (ary[end - 1] > ary[end]) {
+      swap(ary[end - 1], ary[end]);
+    } 
+  }
+}
+
+int medianPart(int* ary, int start, int end)
+{
+  int mid = (start + end) / 2;
+  if (ary[start] > ary[mid]) {
+    swap(ary[start], ary[mid]);
+  }
+
+  if (ary[start] > ary[end]) {
+    swap(ary[start], ary[end]);
+  }
+
+  if (ary[mid] > ary[end]) {
+    swap(ary[mid], ary[end]);
+  }
+
+  swap(ary[mid], ary[end - 1]);
+  return ary[end - 1];
 }
 
 int partition(int* ary, int start, int end, int pivot)
 {
-  int left = start - 1;
-  int right = end + 1;
+  int left = start;
+  int right = end - 1;
 
   while(true) {
     while(ary[++left] < pivot);
-    while(right > 0 && ary[--right] > pivot);
+    while(ary[--right] > pivot);
 
     if (left >= right) {
       break;
@@ -34,6 +76,7 @@ int partition(int* ary, int start, int end, int pivot)
 
     swap(ary[left], ary[right]);
   }
+  swap(ary[left], ary[end - 1]);
   return left;
 }
 
@@ -43,10 +86,15 @@ void quicksort(int* ary, int start, int end)
     return;
   }
 
-  int pivot = ary[end];
+  if (end - start < 3) {
+    manualSort(ary, start, end);
+    return;
+  }
+
+  int pivot = medianPart(ary, start, end);
   int partAt = partition(ary, start, end, pivot);
   quicksort(ary, start, partAt - 1);
-  quicksort(ary, partAt, end);
+  quicksort(ary, partAt + 1, end);
 }
 
 
@@ -54,15 +102,14 @@ int main()
 {
   srand(time(NULL));
 
-  int aryLen = 11;
-  int randRange = 100;
+  int aryLen = 10;
+  int randRange = 1000;
   int ary[aryLen] = {0};
-  int pivotVal = 50;
 
   fillArray(ary, aryLen, randRange);
-  printArray(ary, 0, aryLen);
+  printArray(ary, aryLen);
   quicksort(ary, 0, aryLen - 1);
-  printArray(ary, 0, aryLen);
+  printArray(ary, aryLen);
 
   return 0;
 }
