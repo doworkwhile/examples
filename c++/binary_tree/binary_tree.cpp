3d4146c8d78a27d3c8fd2cd8d4fd1a75c4ce66ee
doworkwhile <doworkwhile@protonmail.com>
2019-05-16 19:23:11 -0700

    c++: binary tree find leaf by value, get depth of value, output sorted vector of values

--- c++/binary_tree/binary_tree.cpp
+++ c++/binary_tree/binary_tree.cpp
@@ -1,23 +1,17 @@
 #include <iostream>
 #include <stdlib.h>
+#include <vector>
 
 using namespace std;
 
 class Leaf
 {
 public:
-  Leaf(int);
+  Leaf(int v) : value(v), smallChild(NULL), bigChild(NULL) {};
   int value;
   Leaf* smallChild;
   Leaf* bigChild;
 };
-
-Leaf::Leaf(int v)
-{
-  this->value = v;
-  this->smallChild = NULL;
-  this->bigChild = NULL;
-}
 
 class BinaryTree
 {
@@ -27,10 +21,14 @@ public:
   Leaf* root;
   void addLeaf(Leaf*);
   void print();
+  Leaf* findLeaf(int);
+  int findDepth(int);
+  vector<int> toSortedVector();
 private:
   void freeLeaf(Leaf*);
   void addFromLeaf(Leaf*, Leaf*);
   void printFromLeaf(Leaf*, int);
+  void collectFromLeaf(vector<int>*, Leaf*);
 };
 
 BinaryTree::~BinaryTree()
@@ -111,6 +109,74 @@ void BinaryTree::printFromLeaf(Leaf* current, int depth)
   }
 }
 
+Leaf* BinaryTree::findLeaf(int v) {
+  if (!root) {
+    return NULL;
+  }
+
+  Leaf* search = root;
+  while (search->value != v) {
+    if (search->value > v) { // leaf to search should be smaller
+      search = search->smallChild;
+    } else { // else the leaf should be bigger
+      search = search->bigChild;
+    }
+
+    // out of nodes?
+    if (!search) {
+      return NULL;
+    }
+  }
+  return search;
+}
+
+int BinaryTree::findDepth(int v) {
+  if (!root) {
+    return -1;
+  }
+
+  Leaf* search = root;
+  int depth = 0;
+  while (search->value != v) {
+    if (search->value > v) { // leaf to search should be smaller
+      search = search->smallChild;
+    } else { // else the leaf should be bigger
+      search = search->bigChild;
+    }
+    ++depth;
+
+    // out of nodes?
+    if (!search) {
+      return --depth;
+    }
+  }
+  return depth;
+}
+
+vector<int> BinaryTree::toSortedVector() {
+  vector<int> collection;
+  if (!root) {
+    return collection;
+  }
+
+  collectFromLeaf(&collection, root);
+  return collection;
+}
+
+void BinaryTree::collectFromLeaf(vector<int>* v, Leaf* current)
+{
+  if (current->smallChild) {
+    collectFromLeaf(v, current->smallChild);
+  }
+
+  v->push_back(current->value);
+
+  if (current->bigChild) {
+    collectFromLeaf(v, current->bigChild);
+  }
+}
+
+
 
 int main()
 {
@@ -128,6 +194,19 @@ int main()
     b.addLeaf(ary[i]);
   }
   b.print();
+
+  int randFindIndex = rand() % leafLen;
+  int valToFind = ary[randFindIndex]->value;
+  cout << "Depth of " << valToFind << ": ";
+  int found = b.findDepth(valToFind);
+  cout << found << endl;
+
+  cout << "Elements in order:" << endl;
+  vector<int> collection = b.toSortedVector();
+  for (int i = 0; i < collection.size(); i++) {
+    cout << collection[i] << " ";
+  }
+  cout << endl;
 
   return 0;
 }
