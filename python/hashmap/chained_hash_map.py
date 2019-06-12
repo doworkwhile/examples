dbbb8bd0c07b1141b36275b85aeb73d4341ecc28
doworkwhile <doworkwhile@protonmail.com>
2019-06-12 12:45:53 -0700

    python finish chained hash map

--- python/hashmap/chained_hash_map.py
+++ python/hashmap/chained_hash_map.py
@@ -1,11 +1,38 @@
+class ChainedHashNode:
+  def __init__(self, key, n, nxt=None):
+    self.key = key
+    self.n = n
+    self.nxt = nxt
+
+  def __repr__(self):
+    return str((self.key, self.n))
+
 class ChainedHashMap:
   def __init__(self, n = 100):
     self.n = n
     self.contents = [[] for i in xrange(n)]
+    self.head = None
 
   def insert(self, key, n):
     i = self.hash_func(key)
-    self.contents[i].append((key, n))
+    newNode = ChainedHashNode(key, n)
+    self.contents[i].append(newNode)
+
+    if not self.head or self.compare(n, self.head.n):
+      newNode.nxt = self.head
+      self.head = newNode
+      return
+
+    current = self.head
+    while current.nxt and self.compare(current.nxt.n, n):
+      current = current.nxt
+    newNode.nxt = current.nxt
+    current.nxt = newNode
+
+
+  def compare(self, left, right):
+    return left > right
+
 
   def hash_func(self, key):
     return hash(key) % self.n
@@ -13,22 +40,19 @@ class ChainedHashMap:
   def __getitem__(self, key):
     i = self.hash_func(key)
     chain = self.contents[i]
-    if len(chain) == 0:
-      return None
-
     for c in xrange(len(chain)):
-      if chain[c][0] == key:
-        return chain[c][1]
+      if chain[c].key == key:
+        return chain[c]
 
-chm = ChainedHashMap()
+chm = ChainedHashMap(5)
 
-keys = []
-for i in xrange(10):
-  key = hash(str(i))
-  keys.append(key)
-  chm.insert(key, i)
+keys = [25, 40, 75, 60, 4, 12, 99]
+for k in keys:
+  chm.insert(k, k)
 
-print chm.contents
-for i in keys:
-  print i, '=', chm[i]
-print 'cat = ', chm['cat']
+current = chm.head
+while current:
+  print current
+  current = current.nxt
+
+print chm[keys[3]]
