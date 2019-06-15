f18180fc00dfa4cd6f461882c35a23755d916dad
doworkwhile <doworkwhile@protonmail.com>
2019-06-14 22:48:23 -0700

    ruby binary tree, some depth functions and DSW

--- ruby/binary_tree/binary_tree.rb
+++ ruby/binary_tree/binary_tree.rb
@@ -102,15 +102,132 @@ class BinaryTree
   end
 
   def find_depth(v)
+    current = @head
+    depth = 0
+    while current
+      if current.value == v
+        return depth
+      elsif v < current.value
+        current = current.left
+      else # v > current.value
+        current = current.right
+      end
+      depth += 1
+    end
+    nil
+  end
 
+  def depth_max
+    depth_max_from(@head)
   end
 
   def depth_count(depth)
-
+    foundAtDepth = 0
+    traverseInOrder(@head, 1, depth) { |node, node_depth|
+      if node_depth == depth
+        foundAtDepth += 1
+      end
+    }
+    return foundAtDepth
   end
 
   def depth_find(depth)
+    setFoundAtDepth = []
+    traverseInOrder(@head, 1, depth) { |node, node_depth|
+      if node_depth == depth
+        setFoundAtDepth << node.value
+      end
+    }
+    return setFoundAtDepth
+  end
 
+  def make_linked_list
+    return unless @head
+    grandparent = nil
+    parent = @head
+    leftChild = nil
+
+    # starting at parent = root
+    while parent
+      leftChild = parent.left
+
+      # if there is a left child
+      if leftChild
+
+        # this is a "right rotation"
+        # move the left child between the grandparent and parent
+        # if there is no grandparent, the left child becomes the head
+        if grandparent
+          grandparent.right = leftChild
+        else
+          @head = leftChild
+        end
+        # now the grandparent.right/head is the left child
+
+        # set the old link to left child to the left childs right child
+        parent.left = leftChild.right
+        # now the left childs right child is the parent
+        leftChild.right = parent
+
+        # what used to be down left, is now up
+        # the old left child could still have more children so move up
+        parent = leftChild
+      else
+        # if no left child
+        # move down right
+        grandparent = parent
+        parent = parent.right
+      end
+    end
+  end
+
+  def make_balanced
+    # first convert to a linked list
+    make_linked_list
+
+    # find the length of the linked list
+    # don't just check the max depth, we don't need to check any left nodes
+    leafCount = 0
+    start = @head
+    while start
+      leafCount += 1
+      start = start.right
+    end
+
+    # EX
+    # leaf_count = 10
+    # count_msb_of = 1011
+    # msb_shifts = 3
+    # rotations_to_half = 7 (1 << 3 = 1000 then 0111 after -1)
+    # first_rotation = 3 (10 - 7)
+    # then 7/2 > 1, rotate floor(7/2) times
+    # then 3/2 > 1, rotate(3/2) times
+    # then 1/2 < 1, finished
+
+    count_msb_of = leafCount + 1
+    msb_shifts = 0
+    while (1 < count_msb_of)
+      count_msb_of = (count_msb_of >> 1)
+      msb_shifts += 1
+    end
+    rotationsToHalf = (1 << msb_shifts) - 1
+
+
+    firstRotation = leafCount - rotationsToHalf
+    balance_rotation(firstRotation)
+
+    while (rotationsToHalf > 1)
+      rotationsToHalf /= 2
+      balance_rotation(rotationsToHalf)
+    end
+
+  end
+
+  def doPrint()
+    traverseInOrder(@head) { |n, d|
+      (d - 1).times { print "---|" }
+      puts "#{n.value}"
+    }
   end
 
   private
@@ -174,5 +291,66 @@ class BinaryTree
       end
     end
     true
+  end
+
+  def depth_max_from(from=nil, mem=0)
+    return mem unless from
+    return [
+      mem + 1,
+      depth_max_from(from.left, mem + 1),
+      depth_max_from(from.right, mem + 1)
+    ].max
+  end
+
+  def traverseInOrder(start, current_depth=1, max_depth=nil)
+    return unless start
+    return if max_depth && current_depth > max_depth
+    traverseInOrder(start.left, current_depth + 1, max_depth) { |e, n|
+      yield(e, n)
+    }
+    yield(start, current_depth)
+    traverseInOrder(start.right, current_depth + 1, max_depth) { |e, n|
+      yield(e, n)
+    }
+  end
+
+  # this is a "left rotation"
+  # move the parent down left of the right child 
+  def balance_rotation(rotations)
+    grandparent = nil
+    parent = @head
+    right_child = @head.right
+
+    # repeat "rotations" # of times
+    while rotations > 0
+      # in case the # of rotations was too much
+      break unless right_child
+
+      # rotate left
+      # move the right child of the parent to the right of the grandparent
+      # (if no grandparent, it becomes the head)
+      if grandparent
+        grandparent.right = right_child
+      else
+        @head = right_child
+      end
+      # the parents right is now the right childs left child
+      parent.right = right_child.left
+      # set the right childs left to the parent
+      right_child.left = parent
+      # end rotate left
+
+      # the new grandparent is the right child
+      grandparent = right_child
+      # the new parent is to the right of the grandparent
+      # which was the old right child (which may not have another right child)
+      parent = grandparent.right
+      # if the old right child had another right child, we use that
+      # for the next iteration
+      right_child = parent ? parent.right : nil
+
+      # that was one rotation
+      rotations -= 1
+    end
   end
 end
--- ruby/binary_tree/test_binary_tree.rb
+++ ruby/binary_tree/test_binary_tree.rb
@@ -2,7 +2,7 @@ require './binary_tree.rb'
 require 'test/unit'
 
 
-def fillSimple(bt)
+def fill_simple(bt)
   bt.add(20)
   bt.add(30)
   bt.add(10)
@@ -14,7 +14,7 @@ end
   7   15  25    40
 5             35  50
 =end
-def fillComplex(bt)
+def fill_complex(bt)
   bt.add(20)
   bt.add(10)
   bt.add(15)
@@ -34,77 +34,170 @@ class TestBinaryTree < Test::Unit::TestCase
   end
 
 
-  def testAdd
+  def test_add
     assert_equal(@bt.head, nil)
-    fillSimple(@bt)
+    fill_simple(@bt)
 
     assert_equal(@bt.head.value, 20)
     assert_equal(@bt.head.right.value, 30)
     assert_equal(@bt.head.left.value, 10)
   end
 
-  def testFind
-    fillSimple(@bt)
+  def test_find
+    fill_simple(@bt)
 
     assert_equal(@bt.find(20).value, 20)
     assert_equal(@bt.find(30).value, 30)
     assert_equal(@bt.find(10).value, 10)
   end
   
-  def testClear
-    fillSimple(@bt)
+  def test_clear
+    fill_simple(@bt)
     assert_equal(@bt.head.value, 20)
     @bt.clear
     assert_equal(@bt.head, nil)
   end
 
-  def testTo_a
-    fillSimple(@bt)
+  def test_to_a
+    fill_simple(@bt)
     assert_equal(@bt.to_a, [10, 20, 30])
 
     @bt.clear
-    fillComplex(@bt)
+    fill_complex(@bt)
     assert_equal(@bt.to_a, [5, 7, 10, 15, 20, 25, 30, 35, 40, 50])
   end
 
-  def testTo_a_preorder
-    fillSimple(@bt)
+  def test_to_a_preorder
+    fill_simple(@bt)
     assert_equal(@bt.to_a_preorder, [20, 10, 30])
 
     @bt.clear
-    fillComplex(@bt)
+    fill_complex(@bt)
     assert_equal(@bt.to_a_preorder, [20, 10, 7, 5, 15, 30, 25, 40, 35, 50])
   end
 
-  def testTo_a_postorder
-    fillSimple(@bt)
+  def test_to_a_postorder
+    fill_simple(@bt)
     assert_equal(@bt.to_a_postorder, [10, 30, 20])
 
     @bt.clear
-    fillComplex(@bt)
+    fill_complex(@bt)
     assert_equal(@bt.to_a_postorder, [5, 7, 15, 10, 25, 35, 50, 40, 30, 20])
   end
 
 
-  def testDelete
-    fillComplex(@bt)
+  def test_delete
+    fill_complex(@bt)
     @bt.delete(5)
     assert_equal(@bt.to_a, [7, 10, 15, 20, 25, 30, 35, 40, 50])
 
     @bt.clear
-    fillComplex(@bt)
+    fill_complex(@bt)
     @bt.delete(10)
     assert_equal(@bt.to_a, [5, 7, 15, 20, 25, 30, 35, 40, 50])
 
     @bt.clear
-    fillComplex(@bt)
+    fill_complex(@bt)
     @bt.delete(30)
     assert_equal(@bt.to_a, [5, 7, 10, 15, 20, 25, 35, 40, 50])
 
     @bt.clear
-    fillComplex(@bt)
+    fill_complex(@bt)
     @bt.delete(20)
     assert_equal(@bt.to_a, [5, 7, 10, 15, 25, 30, 35, 40, 50])
+  end
+
+  def test_find_depth
+    fill_complex(@bt)
+
+    # get the list of items in ascending order
+    ordered_items = @bt.to_a
+
+    # map the depth of every item
+    depths = ordered_items.map { |e| @bt.find_depth(e) }
+
+    # compare to pre-calculated list
+    assert_equal(depths, [3, 2, 1, 2, 0, 2, 1, 3, 2, 3])
+  end
+
+  def test_depth_max
+    assert_equal(@bt.depth_max, 0)
+
+    @bt.add(1)
+    assert_equal(@bt.depth_max, 1)
+
+    @bt.clear
+    fill_simple(@bt)
+    assert_equal(@bt.depth_max, 2)
+
+    @bt.clear
+    fill_complex(@bt)
+    assert_equal(@bt.depth_max, 4)
+  end
+
+  def test_depth_count
+    fill_complex(@bt)
+
+    # for each depth, get the count of nodes
+    @bt.depth_count(@head)
+    depth_counts = [*1..@bt.depth_max].map { |e| @bt.depth_count(e) }
+
+    # compare to pre-calculated list
+    assert_equal(depth_counts, [1, 2, 4, 3])
+  end
+
+  def test_depth_find
+    assert_equal(@bt.depth_find(0), [])
+    assert_equal(@bt.depth_find(1), [])
+
+    @bt.add(1)
+    assert_equal(@bt.depth_find(0), [])
+    assert_equal(@bt.depth_find(1), [1])
+
+    @bt.clear
+    fill_simple(@bt)
+    assert_equal(@bt.depth_find(1), [20])
+    assert_equal(@bt.depth_find(2), [10, 30])
+
+    @bt.clear
+    fill_complex(@bt)
+    assert_equal(@bt.depth_find(1), [20])
+    assert_equal(@bt.depth_find(2), [10, 30])
+    assert_equal(@bt.depth_find(3), [7, 15, 25, 40])
+    assert_equal(@bt.depth_find(4), [5, 35, 50])
+  end
+
+  def test_make_linked_list
+    fill_complex(@bt)
+
+    ary = @bt.to_a
+    i = 0
+
+
+    @bt.make_linked_list
+    # test the sorted linked list by traversing and comparing indexes
+    # to a saved sorted array
+    current = @bt.head
+    while current
+      assert_equal(ary[i], current.value)
+      current = current.right
+      i += 1
+    end
+  end
+
+  def test_make_balanced
+    fill_simple(@bt)
+    @bt.make_balanced
+
+    @bt.clear
+    fill_complex(@bt)
+    @bt.make_balanced
+    assert_equal(@bt.depth_max, 4)
+
+    @bt.clear()
+    (0...100).each { |i| @bt.add(i) }
+    @bt.make_balanced
+    assert_equal(@bt.depth_max, 7)
   end
 
 
