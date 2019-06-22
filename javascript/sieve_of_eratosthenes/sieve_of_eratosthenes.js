2e8c5740cc7ef689930188bbeda29a55217205f6
doworkwhile <doworkwhile@protonmail.com>
2019-06-22 14:56:17 -0700

    python sieve of eratosthenes

--- javascript/sieve_of_eratosthenes/sieve_of_eratosthenes.js
+++ javascript/sieve_of_eratosthenes/sieve_of_eratosthenes.js
@@ -1,18 +1,18 @@
-var sieve = function (n) {
+const sieve = function (n) {
 
   // no primes besides 1 below 2
-  var prime_minimum = 2;
+  const prime_minimum = 2;
 
   // all composite numbers greater than this have smaller divisors
-  var prime_maximum = Math.sqrt(n);
+  const prime_maximum = Math.sqrt(n);
 
   // the arrays are 0-indexed and we start at 2 so ..
   // the size of the array is n-1 to exclude the digit '1'
-  var list = new Array(n - 1).fill(true);
+  const list = new Array(n - 1).fill(true);
 
   // 'i' here represents the integer whose multiples are obviously composite
   // 'i' is NOT THE ARRAY INDEX
-  for (var i = prime_minimum; i < prime_maximum; i++) {
+  for (let i = prime_minimum; i < prime_maximum; i++) {
     // again, the arrays are 0-indexed and we start at 2 so ..
     // the digit '2' is at index 0, so the index of any integer is really i-2
 
@@ -23,19 +23,19 @@ var sieve = function (n) {
       // mark the multiple of i as composite ( but not i )
       // by setting its related index to false
       // (we will filter all false values)
-      for(var j = i*i; j <= n; j += i) {
-        list[j - 2] = false
+      for(let j = i*i; j <= n; j += i) {
+        list[j - 2] = false;
       }
     }
   }
 
-  
+
   return list
     // each index of the array represents the digit index+2
-    .map( (e,i) => !!e ? i+2 : false )
+    .map( (e,i) => e ? i+2 : false )
 
     // filter the indexes marked false
-    .filter(e => !!e)
-}
+    .filter(e => !!e);
+};
 
-console.log( sieve(100) );
+module.exports = sieve;
---
+++ python/sieve_of_eratosthenes/sieve_of_eratosthenes.py
@@ -0,0 +1,17 @@
+import math
+
+def sieve(n):
+  primeMinimum = 2
+  primeMaximum = int(math.sqrt(n))
+  numbers = [True for i in range(n - 1)]
+
+  for i in xrange(primeMinimum, primeMaximum):
+    if (numbers[i - primeMinimum]):
+      for j in xrange(i*i, n, i):
+        numbers[j - primeMinimum] = False
+
+  numbers = [(i + primeMinimum if x else False) for i, x in enumerate(numbers)]
+
+  return filter(lambda x: x != False, numbers)
+
+print sieve(100)
